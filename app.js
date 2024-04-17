8435feb22f4455806bc05a4b2e9f54d5e6d69dde
vincentastolfi <vincent.astolfi@etu.uca.fr>
2024-04-17 19:19:53 +0200

    :green_heart: CI test

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
-const { io } = require(`${__dirname}/index.js`);
 const { Player } = require(`${__dirname}/businesses/Player.js`);
 const { Room } = require(`${__dirname}/businesses/Room.js`);
 const { Game } = require(`${__dirname}/game.js`);
+const { io } = require(`${__dirname}/index.js`);
 
 let room;
 let game;
 
 io.on("connection", (socket) => {
-  /* TODO : handle disconnection
-  socket.on("disconnect", (reason) => {
-    if (game.room.players.length === 2) {
-      game.endGame();
-      delete game.room;
-    }
-    console.debug(game);
-  });
-  */
-
   socket.on("Hello", (callback) => {
     callback({
       Hello: "World",
--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const http = require("http").createServer(app);
 const path = require("path");
 const port = 8081;
 const io = require("socket.io")(http);
+const { Player } = require(`${__dirname}/businesses/Player.js`);
+const { Room } = require(`${__dirname}/businesses/Room.js`);
+const { Game } = require(`${__dirname}/game.js`);
+
+let room;
+let game;
 
 app.use(express.static("public"));
 
@@ -13,6 +19,36 @@ http.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+io.on("connection", (socket) => {
+  socket.on("Hello", (callback) => {
+    callback({
+      Hello: "World",
+    });
+  });
+
+  socket.on("room creation", (socketId) => {
+    let player = new Player(socketId);
+    player.grid.cases[2][2].isShip = true;
+    player.grid.cases[3][2].isShip = true;
+    room = new Room();
+    room.addPlayer(player);
+  });
+
+  socket.on("ask for room", (socketId) => {
+    if (room.players.length === 1) {
+      let player = new Player(socketId);
+      player.grid.cases[2][2].isShip = true;
+      room.addPlayer(player);
+      game = new Game(room);
+      game.start();
+    }
+  });
+
+  socket.on("play", (move) => {
+    game.move(move);
+  });
 });
 
 module.exports = {
