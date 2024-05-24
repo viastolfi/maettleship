b0f5a9b1291c794bfc3223a1e5b13f16e516f200
vincentastolfi <vincent.astolfi@etu.uca.fr>
2024-05-24 08:49:41 +0200

    :green_heart: new CD test with different server instance

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 const port = 8081;
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://codefirst.iut.uca.fr/containers/vincentastolfi-maettleship",
+    // or with an array of origins
+    // origin: ["https://my-frontend.com", "https://my-other-frontend.com", "http://localhost:3000"],
+    credentials: true
+  }
+});
 
 app.use(express.static("public"));
 
