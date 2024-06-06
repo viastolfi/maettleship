773b4fbd22950dbe08e98a26eb4be8f1a461cc66
vincentastolfi <vincent.astolfi@etu.uca.fr>
2024-06-06 09:51:39 +0200

    :green_heart: test if codefirst only deploy port 80

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-const port = 8081;
+const port = 80;
 
 app.use(express.static("public"));
 
