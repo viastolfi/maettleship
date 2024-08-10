15b3f99895763499d358535a894755befe1b7bee
vincentastolfi <vincent.astolfi@etu.uca.fr>
2024-08-10 12:50:55 +0200

    :card_file_box: change db creation scripts

--- db/init.sql
+++ db/init.sql
@@ -2,8 +2,9 @@ CREATE DATABASE IF NOT EXISTS battleship;
 
 USE battleship;
 CREATE TABLE IF NOT EXISTS users (
-    pseudo VARCHAR(256),
-    password VARCHAR(256)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    pseudo VARCHAR(255) UNIQUE NOT NULL,
+    hashed_password VARCHAR(255) NOT NULL
 );
 
 CREATE USER 'root' IDENTIFIED WITH mysql_native_password BY 'rootpassword';
