e36d82e472803c5bb2d712ce5b900898efe4e283
viastolfi <vincent.astolfi12@gmail.com>
2024-08-13 19:43:31 +0200

    rename directory

--- database.js
+++ database.js
@@ -7,18 +7,4 @@ const connection = mysql.createPool({
     database: process.env.DB_NAME
 });  
 
-/*
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database');
-});
-*/
-
 module.exports = connection;
-
-// TODO
-// Change how i insert in my db so player1 = player1Win
-// Maybe store the players positions in the database directly in the room object
