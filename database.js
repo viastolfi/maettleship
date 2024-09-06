56859467fe4edc5978737355284eae329c4824eb
Vincent <dev@srv576770.hstgr.cloud>
2024-09-06 07:38:02 +0000

    :sparkles: Secrets working well. Better security overall

--- database.js
+++ database.js
@@ -1,10 +1,23 @@
 const mysql = require("mysql2");
+const fs = require('node:fs');
 
-const connection = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});  
 
-module.exports = connection;
+try {
+	const db_user_password = fs.readFileSync(process.env.DB_USER_PASSWORD_FILE, 'utf8').replace(/\r?\n|\r/g, "");
+	const db_user = fs.readFileSync(process.env.DB_USER_FILE, 'utf8').replace(/\r?\n|\r/g, "");
+
+
+	console.log(db_user)
+
+	const connection = mysql.createPool({
+   		host: process.env.DB_HOST,
+    	user: db_user,
+		password: db_user_password,
+    	database: process.env.DB_NAME,
+	});  
+
+	module.exports = connection;
+} catch (e) {
+	console.log(e)
+}
+
